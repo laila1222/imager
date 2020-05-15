8bfdeb5ef3b88a17385fe745e4c9e26a260baaac
Laila1222 <korosililla@gmail.com>
2020-05-15 10:54:39 +0200

    created pages folder and home page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,29 @@
 import React, { Component } from "react";
-import SearchBar from "./components/SearchBar/SearchBar";
-import ImageDisplay from "./components/ImageDisplay/ImageDisplay";
-import axios from "axios";
+import Home from './pages/Home';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import "./css/style.css";
-// ES Modules syntax
-import Unsplash from "unsplash-js";
 
-const accessKey = process.env.REACT_APP_ACCESS_KEY;
-
-const unsplash = new Unsplash({ accessKey });
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      imagesArray: [],
-      searchWord: "",
-    };
   }
 
-  // For child parent communication
-  // When child (input) calls this function, change state.searchWord, then search for images with the new searchWord
-  handler = (inputValue) => {
-    console.log(inputValue);
-    this.setState({ searchWord: inputValue }, () => {
-      this.searchForImage(this.state.searchWord);
-    });
-  };
-
-  searchForImage = (searchWord) => {
-    unsplash.search
-      .photos(searchWord, 1, 10, { orientation: "portrait" })
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data.results);
-        this.setState({ imagesArray: data.results });
-      });
-  };
-
-  componentDidMount = () => {
-    const word = "nature";
-    // this.searchForImage(word);
-  };
+ 
 
   render() {
     return (
-      <React.Fragment>
-        <SearchBar handler={this.handler} imagesDisplayed={this.state.imagesDisplayed} />
-        <ImageDisplay imagesArray={this.state.imagesArray} />
-      </React.Fragment>
+      <div>
+        <Home />
+      </div>
     );
   }
 }
